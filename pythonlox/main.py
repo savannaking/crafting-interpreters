40c7d768581dce150dbebb2587f64f8a2311d316
Savanna King <savanna18king@gmail.com>
2024-05-23 10:48:28 -0500

    added ast generator

--- pythonlox/main.py
+++ pythonlox/main.py
@@ -1,13 +1,14 @@
 import sys
 from scanner.scanner import Scanner
 
-def main(): 
-    if (len(sys.argv) > 1):
+
+def main():
+    if len(sys.argv) > 2:
         print("PythonLox accepts only 0 or 1 command line arguments.")
         print("To run the REPL, use no arguments. To run a file, supply the file name.")
-    elif (sys.argv.count == 1):
-        runFile(sys.argv[0])
-    else: 
+    elif sys.argv.count == 1:
+        runFile(sys.argv[1])
+    else:
         runPrompt()
 
 
@@ -15,14 +16,16 @@ def runFile(path: str):
     # implement this later
     return
 
+
 def runPrompt():
-    while(True):
+    while True:
         print(">> ")
         line = input()
-        if (line == None):
+        if line == None:
             break
         run(line)
         hadError = False
+
 
 def run(src: str):
     scanner = Scanner(src)
